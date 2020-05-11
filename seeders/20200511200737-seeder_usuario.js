e40e52694bade081a75b8d89e5ff458a9018d449
unknown <Digital@Comodato.dhbrasil.digitalhouse.com>
2020-05-11 17:24:32 -0300

    adicionando seeders e bcrypt

--- seeders/20200511200737-seeder_usuario.js
+++ seeders/20200511200737-seeder_usuario.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -12,6 +13,23 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    return queryInterface.bulkInsert('usuario', [
+      {
+        nome: 'John Doe',
+        email: 'johndoe@mail.com',
+        senha: bcrypt.hashSync('123456', 10)
+      },
+      {
+        nome: 'Murilo',
+        email: 'murilo@mail.com',
+        senha: bcrypt.hashSync('1234567', 10)
+      },
+      {
+        nome: 'Larissa',
+        email: 'larry@mail.com',
+        senha: bcrypt.hashSync('1234a756', 10)
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +40,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+   return queryInterface.bulkDelete('usuario', null, {});
   }
 };
